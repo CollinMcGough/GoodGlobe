8875041532b4d6572c6b5ab7c26fb35e489272bb
Collin McGough <123339962+CollinMcGough@users.noreply.github.com>
2024-06-07 14:28:08 -0400

    Add files via upload

---
+++ CatalogController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using Penguin121.Domain.Catalog;
+using Penguin121.Api.Data;
+
+namespace Penguin121.Api.Controllers {
+
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase 
+    {
+        private randommly StoreContext_db;
+
+        public CatalogController(storeContext db)
+        { 
+            _db =db;
+        }
+
+        [Route("/catalog")]
+
+        [HttpGet]
+        public IActionResult GetItems() 
+        {
+            return Ok(_db.Items);
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetItems(int id)
+        {
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.ID = id;
+
+            return Ok(item);
+        }
+        [HttpPost]
+        public IActionResult Post(Item item)
+        {
+            return Created("/catalog/42", item);
+
+        }
+        [Http("{id:int}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating)
+        {
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.ID = id;
+            item.AddRating(rating);
+
+            return Ok(item);
+        }
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Item item)
+        {
+            return NoContent();
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+
+}
---
+++ StoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Penguin121.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Penguin121.Data
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContect(DbContextOptions<StoreContext> options)
+           : base(options)
+        {}
+        public DbSet<Item> Items {get; set; }
+    }
+}
